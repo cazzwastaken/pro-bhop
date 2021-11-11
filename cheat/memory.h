f76c21124113be66d2c4387ae0613dce9ec7e663
cazzwastaken <cazzwastaken@gmail.com>
2021-11-11 16:20:40 +0200

    made memory class header only

--- cheat/memory.h
+++ cheat/memory.h
@@ -1,6 +1,9 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <iostream>
+#include <TlHelp32.h>
+
+#include <cstdint>
 
 class Memory
 {
@@ -9,24 +12,74 @@ private:
 	HANDLE process = nullptr;
 
 public:
-	Memory(const char* processName);
-	~Memory();
+	// Constructor that finds the process id
+	// and opens a handle
+	Memory(const char* processName)
+	{
+		PROCESSENTRY32 entry;
+		entry.dwSize = sizeof(PROCESSENTRY32);
 
-	constexpr std::uintptr_t ProcessId() { return this->id; }
+		const auto snapShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	std::uintptr_t GetModuleAddress(const char* moduleName);
+		while (Process32Next(snapShot, &entry))
+		{
+			if (!strcmp(processName, entry.szExeFile))
+			{
+				id = entry.th32ProcessID;
+				process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, id);
+				break;
+			}
+		}
 
+		if (snapShot)
+			CloseHandle(snapShot);
+	}
+
+	// Destructor that frees the opened handle
+	~Memory()
+	{
+		if (process)
+			CloseHandle(process);
+	}
+
+	// Returns the base address of a module by name
+	const std::uintptr_t GetModuleAddress(const char* moduleName)
+	{
+		MODULEENTRY32 entry;
+		entry.dwSize = sizeof(MODULEENTRY32);
+
+		const auto snapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, this->id);
+
+		std::uintptr_t result = 0;
+
+		while (Module32Next(snapShot, &entry))
+		{
+			if (!strcmp(moduleName, entry.szModule))
+			{
+				result = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				break;
+			}
+		}
+
+		if (snapShot)
+			CloseHandle(snapShot);
+
+		return result;
+	}
+
+	// Read process memory
 	template <typename T>
 	constexpr T Read(std::uintptr_t address) const noexcept
 	{
 		T value{ };
-		ReadProcessMemory(this->process, reinterpret_cast<LPCVOID>(address), &value, sizeof(T), NULL);
+		ReadProcessMemory(process, reinterpret_cast<LPCVOID>(address), &value, sizeof(T), NULL);
 		return value;
 	}
 
+	// Write process memory
 	template <typename T>
 	constexpr const bool Write(std::uintptr_t address, T value) const noexcept
 	{
-		return WriteProcessMemory(this->process, reinterpret_cast<LPVOID>(address), &value, sizeof(T), NULL);
+		return WriteProcessMemory(process, reinterpret_cast<LPVOID>(address), &value, sizeof(T), NULL);
 	}
 };
